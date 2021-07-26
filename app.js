7e338f5ca4927c0be9cfad54faf66bde978017e5
lcx12901 <1367372276@qq.com>
2021-07-26 16:48:43 +0800

    初始化加载时检查登录状态

---
+++ app.js
@@ -0,0 +1,19 @@
+// app.js
+App({
+  onLaunch() {
+    // 检查是否登录
+    if (!wx.getStorageSync('profile')) {
+      wx.showToast({
+        title: '请先进行登录',
+        duration: 1500,
+        image: 'assets/imgs/login.png',
+        success () {
+          wx.navigateTo({
+            url: '/pages/login/login'
+          })
+        }
+      })
+      
+    }
+  }
+})
