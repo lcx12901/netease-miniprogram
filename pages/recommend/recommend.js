b205328499ec7b659d882ace3b02f17f7a71ccbe
lcx12901 <1367372276@qq.com>
2021-07-27 00:01:25 +0800

    创建每日推荐页面

---
+++ pages/recommend/recommend.js
@@ -0,0 +1,72 @@
+// pages/recommend/recommend.js
+import {reqRecommendSongs} from '../../network/api'
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+  async getRecommendSongs () {
+    const {data} = await reqRecommendSongs ()
+    this.setData({
+      recommends: data
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getRecommendSongs()
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
