86ec38ae37a09cb7462829c8a7747068c5373c8d
lcx12901 <1367372276@qq.com>
2021-07-27 00:01:25 +0800

    更新api接口

--- network/api.js
+++ network/api.js
@@ -5,3 +5,9 @@ export const reqLogin = (phone,password) => request({url:'/login/cellphone',data
 
 // 获取首页banner图
 export const reqBanner = () => request({url: '/banner', data: {type: 2}})
+
+// 首页-发现-圆形图标入口列表
+export const reqHomePageBall = () => request({url: '/homepage/dragon/ball'})
+
+// 获取每日推荐歌曲
+export const reqRecommendSongs = () => request({url: '/recommend/songs'})
