dc0f633d61769ef88a0fc17cac11247d616a2f92
lcx12901 <136737276@qq.com>
2021-07-29 17:49:14 +0800

    首次loading检测登录状态

--- app.js
+++ app.js
@@ -2,17 +2,17 @@
 App({
   onLaunch() {
     // 检查是否登录
-    // if (!wx.getStorageSync('profile')) {
-    //   wx.showToast({
-    //     title: '请先进行登录',
-    //     duration: 1500,
-    //     image: 'assets/imgs/login.png',
-    //     success () {
-    //       wx.navigateTo({
-    //         url: '/pages/login/login'
-    //       })
-    //     }
-    //   })
-    // }
+    if (!wx.getStorageSync('profile')) {
+      wx.showToast({
+        title: '请先进行登录',
+        duration: 1500,
+        image: 'assets/imgs/login.png',
+        success () {
+          wx.navigateTo({
+            url: '/pages/login/login'
+          })
+        }
+      })
+    }
   }
 })
