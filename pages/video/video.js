240a1382a3af22fcac26822c72da0a89adfffad3
lcx12901 <136737276@qq.com>
2021-07-29 17:50:52 +0800

    视频页面

--- pages/video/video.js
+++ pages/video/video.js
@@ -1,4 +1,5 @@
 // pages/video/video.js
+import {reqVideoGroupList, reqVideoGroup} from '../../network/api.js'
 Page({
 
     /**
@@ -7,12 +8,34 @@ Page({
     data: {
 
     },
-
+    async getVideoGroupList () {
+        const {data:videoGroupList} = await reqVideoGroupList()
+        this.setData({
+            groupId: videoGroupList[0].id,
+            videoGroupList: videoGroupList.slice(0, 10)
+        })
+        this.getVideoGroup(videoGroupList[0].id)
+    },
+    async getVideoGroup (id) {
+        const {datas:videoList} = await reqVideoGroup(id)
+        this.setData({
+            videoList,
+            videoId: videoList[0].data.vid
+        })
+    },
+    
+    // 导航栏点击事件
+    itemClick (e) {
+        const {id} = e.currentTarget.dataset
+        this.setData({
+            groupId: id
+        })
+    },
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function (options) {
-
+    onLoad: function (option) {
+        this.getVideoGroupList()
     },
 
     /**
