c1ebdb48ad374f36a31ecf2c32a817a68bb948fb
lcx12901 <1367372276@qq.com>
2021-08-01 18:12:14 +0800

    监听globalData变化，实时更新页面数据

--- pages/songPlay/songPlay.js
+++ pages/songPlay/songPlay.js
@@ -8,6 +8,44 @@ Page({
     data: {
         audioPlaying: false
     },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        this.calcNavTitle()
+        app.watch(this.watchBack)
+    },
+    // 监听app.js
+    watchBack (value) {
+        const {currentPosition, duration} = value
+        const minute = (`0${Math.floor(duration / 60)}`).slice(-2)
+        const second = (`0${Math.floor(duration % 60)}`).slice(-2)
+        const currentMinute = (`0${Math.floor( (currentPosition + 1) / 60)}`).slice(-2)
+        const currentSecond = (`0${Math.floor( (currentPosition + 1) % 60)}`).slice(-2)
+        this.setData({
+            audioPlaying: value.audioPlaying,
+            song: value.playList.filter(item => item.id == value.songId),
+            progress: ((currentPosition / duration) * 100).toFixed(0),
+            currentPosition: `${currentMinute}:${currentSecond}`,
+            duration: `${minute}:${second}`
+        })
+    },
+    // 计算标题栏的高度
+    calcNavTitle () {
+        const that = this
+        wx.getSystemInfo({
+            success (res) {
+                let menuButton = wx.getMenuButtonBoundingClientRect()
+                let titleBarHeight = (menuButton.top - res.statusBarHeight) * 2 + menuButton.height // 标题栏高
+                that.setData({
+                    safeTop: res.safeArea.top,
+                    titleBarHeight,
+                    titleHeight: menuButton.height
+                })
+            }
+        })
+    },
+    // 获取到globalData的播放列表
     getSongDetail () {
         const song = app.globalData.playList.filter(item => item.id == app.globalData.songId)
         this.setData({
@@ -15,13 +53,28 @@ Page({
             audioPlaying: app.globalData.audioPlaying
         })
     },
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
+    // 返回上一页面
+    back () {
+        wx.navigateBack({
+          delta: 1,
+        })
     },
-
+    // 歌曲播放控制按钮
+    playControll () {
+        if (this.data.audioPlaying) {
+            app.globalData.BackgroundAudioManager.pause()
+        } else {
+            app.globalData.BackgroundAudioManager.play()
+        }
+    },
+    // 切换上一首
+    prevAudio () {
+        app.changeAudio('prev')
+    },
+    // 切换下一首
+    nextAudio () {
+        app.changeAudio('next')
+    },
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
