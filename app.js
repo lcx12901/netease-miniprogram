c1870e3c92d51a290b2e58fa205d0616769bd47c
lcx12901 <1367372276@qq.com>
2021-08-01 18:12:14 +0800

    全局音频管理器&&监听globalData变化返回给其他页面

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // app.js
 import {reqSongUrl} from '/network/api'
-
 App({
   onLaunch() {
+    // 监听本页面修改globalData，使其他页面实时获得
+    this.watch()
+
     // 检查是否登录
     if (!wx.getStorageSync('profile')) {
       wx.showToast({
@@ -15,21 +17,67 @@ App({
           })
         }
       })
-    }
+    };
     // 全局创建背景音频管理器
-    this.globalData.BackgroundAudioManager = wx.getBackgroundAudioManager()
+    this._globalData.BackgroundAudioManager = wx.getBackgroundAudioManager()
+    // 监听背景音乐暂停
+    this.globalData.BackgroundAudioManager.onPause( () => {
+      this.globalData = {
+        audioPlaying: false
+      }
+    })
+    // 监听背景音乐开始
+    this.globalData.BackgroundAudioManager.onPlay( () => {
+      this.globalData = {
+        audioPlaying: true
+      }
+    })
+    setInterval( () => {
+      wx.getBackgroundAudioPlayerState({
+        success: (result) => {
+          const{currentPosition, duration, status} = result
+          // 0 暂停中
+          // 1 播放中
+          // 2 没有音乐播放
+          if (status == 1) {
+            this.globalData = {
+              currentPosition,
+              duration
+            }
+          }
+        },
+      })
+    }, 1000)
     // 自动连播
     this.globalData.BackgroundAudioManager.onEnded(()=> {
-      const len = this.globalData.playList.length
-      if (len) {
-        let index = this.globalData.playList.findIndex(item => item.id === this.globalData.songId)
-        if (len - 1 < index ) return
-        this.playAudio(index+1)
+      this.changeAudio()
+    })
+  },
+  _globalData: {
+    playList: [],
+    songId: 0,
+    audioPlaying: false
+  },
+  // 监听globalData变化
+  watch(method) {
+    Object.defineProperty(this, "globalData", {  // 对其他页面使用globalData进行监听
+      configurable: true,
+      enumerable: true,
+      set: (value) => {  //动态赋值，传递对象，为 globalData 中对应变量赋值
+        Object.keys(value).forEach(item => {
+          this._globalData[item] = value[item]
+        })
+        method(this._globalData)
+      },
+      get: () => {
+        //获取全局变量值，直接返回全部
+        return this._globalData
       }
     })
   },
+  // 播放音频
   async playAudio (index) {
-    const {id, name, al, ar} = this.globalData.playList[index]
+    const {id, name, al, ar} = this._globalData.playList[index]
     const {data, code} = await reqSongUrl(id)
     if (code == 200) {
       this.globalData.songId = id
@@ -39,31 +87,21 @@ App({
       this.globalData.BackgroundAudioManager.coverImgUrl  = al.picUrl
       this.globalData.BackgroundAudioManager.src = data[0].url
       this.globalData.audioPlaying = true
+      return true
     }
   },
-  
-  globalData: {
-    methodList: [], //监听者列表
-    playList: [],
-    audioPlaying: false
-  },
-  //app 全局属性监听
-  watch: function (method) {
-    var obj = this.globalData;
-    obj.methodList.push(method); //加入到监听者列表中
-
-    Object.defineProperty(this, "globalData", {  //这里的 data 对应 上面 globalData 中的 data
-      configurable: true,
-      enumerable: true,
-      set: function (value) {  //动态赋值，传递对象，为 globalData 中对应变量赋值
-        obj.orderNum = value.orderNum;
-        obj.methodList.forEach(element => {
-          element(value); // 遍历监听者列表，回调
-        });
-      },
-      get: function () {  //获取全局变量值，直接返回全部
-        return obj;
+  // 歌曲change
+  changeAudio (call) {
+    const len = this.globalData.playList.length - 1 
+    if (len) {
+      let index = this.globalData.playList.findIndex(item => item.id === this.globalData.songId)
+      if (call == 'prev') {
+        if (index -- <= 0) index = len
       }
-    })
+      else if (call == 'next' || !call) {
+        if ( ++index > len) index = 0 // 如果当前这一首歌为最后一首，从头开始播放
+      }
+      this.playAudio(index)
+    }
   }
 })
